06620158bcbbf44c02a14c761a8776bf91d798db
dileep0553 <dileep.pasumarti@gmail.com>
2025-12-17 20:00:33 -0600

    Added GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <Script id="google-analytics">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5KL2RT32');
+
+
+        `}
+        </Script>
+      </head>
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-HCZKVX7T4F"
@@ -40,9 +53,19 @@ export default function RootLayout({
 
         `}
       </Script>
+      <Script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></Script>
       <body
         className={`${inter.variable} bg-gray-50 font-inter tracking-tight text-gray-900 antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5KL2RT32"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
           {children}
         </div>
