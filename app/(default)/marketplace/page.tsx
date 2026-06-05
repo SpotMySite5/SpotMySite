e6c4163b8a86795604b734b6807403584474c7d8
SpotMySite5 <spotmysite5@gmail.com>
2026-06-05 19:53:46 +0530

    Update page.tsx

--- app/(default)/marketplace/page.tsx
+++ app/(default)/marketplace/page.tsx
@@ -1569,7 +1569,7 @@ const articles = [
     ],
   },
   images: [] as string[],
-  propertyImage: "/images/property/property-16/image_1.jpeg",
+  propertyImage: "/images/property/property-16/image_1.jpg",
   url: "/marketplace/property-16",
   content: [
     {
